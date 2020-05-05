ea065a9f7f3d4cf5d22b405c39adaf9b414f02bc
Dallin Lovin <lowfire3@yahoo.com>
2020-05-05 15:05:44 -0600

    Added more functionality

--- web/03_teach/03_teach.php
+++ web/03_teach/03_teach.php
@@ -5,6 +5,16 @@ $majors = [
   'cit' => 'Computer Information Technology',
   'ce' => 'Computer Engineering'
 ];
+
+$countries = [
+  "na" => "North America",
+  "sa" => "South America",
+  "eu" => "Europe",
+  "as" => "Asia",
+  "au" => "Australia",
+  "af" => "Africa",
+  "an" => "Antartica"
+]
 ?>
 
 
@@ -26,23 +36,21 @@ $majors = [
         <?php foreach($majors as $code => $name): ?>
           <input type="radio" id="<?= $code; ?>"  name="major" value="<?= $name; ?>"><?= $name; ?><br>
         <?php endforeach; ?>
-        <!--
-        <input type="radio" id="cs"  name="major" value="Computer Science"> Computer Science<br>
-        <input type="radio" id="wdd" name="major" value="Web Design and Development">Web Design and Development<br>
-        <input type="radio" id="cit" name="major" value="Computer Information Technology">Computer Information Technology<br>
-        <input type="radio" id="ce"  name="major" value="Computer Engineering">Computer Engineering<br>
-        -->
+
         <label for="comments">Comments:</label><br> 
-        <textarea name="comments"></textarea> <br>
+        <textarea name="comments"></textarea> <br><br>
         
         Visited Countries:<br>
-        <input type="checkbox" name="visited[na]" value="North America"> <label for="North America">North America</label><br>
+        <?php foreach($countries as $code => $name): ?>
+          <input type="checkbox" id="<?= $code; ?>"  name="visited[<?= $code ?>]" value="<?= $name; ?>"><?= $name; ?><br>
+        <?php endforeach; ?>
+        <!-- <input type="checkbox" name="visited[na]" value="North America"> <label for="North America">North America</label><br>
         <input type="checkbox" name="visited[sa]" value="South America"> <label for="South America">South America</label><br>
         <input type="checkbox" name="visited[eu]" value="Europe"> <label for="Europe">Europe</label><br>
-        <input type="checkbox" name="visited[asia]" value="Asia"> <label for="Asia">Asia</label><br>
+        <input type="checkbox" name="visited[as]" value="Asia"> <label for="Asia">Asia</label><br>
         <input type="checkbox" name="visited[au]" value="Australia"> <label for="Australia">Australia</label><br>
-        <input type="checkbox" name="visited[africa]" value="Africa"> <label for="Africa">Africa</label><br>
-        <input type="checkbox" name="visited[ant]" value="Antarctica"> <label for="Antarctica">Antarctica</label><br>
+        <input type="checkbox" name="visited[af]" value="Africa"> <label for="Africa">Africa</label><br>
+        <input type="checkbox" name="visited[an]" value="Antarctica"> <label for="Antarctica">Antarctica</label><br> -->
         <input type="submit" value="Send Details">
     </form>
 </body>
