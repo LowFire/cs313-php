a6730224630be8ece4ad1be9bda2f5940fc37ea7
Dallin Lovin <lowfire3@yahoo.com>
2020-05-28 14:25:19 -0600

    commit

--- web/06_prove/processAddEvent.php
+++ web/06_prove/processAddEvent.php
@@ -3,21 +3,25 @@
     require "getDB.php";
 
     $db = getDB();
-    var_dump($_POST);
-    var_dump(intval($_POST['hr']));
-    var_dump(intval($_POST['min']));
 
     if (!isempty($_POST)) {
         $stmt = $db->prepare("INSERT INTO calendar(eventname, eventdesc, eventdate, eventhr, eventmin, eventabbriv, user_id) 
         VALUES (:eventname, :eventdesc, :eventdate, :eventhr, :eventmin, :eventabbriv, :user_id)");
 
-        // $stmt->bindValue(":eventname", $_POST['eventName'], PDO::PARAM_STR);
-        // $stmt->bindValue(":eventdesc", $_POST['eventDesc'], PDO::PARAM_STR);
-        // $stmt->bindValue(":eventdate", $_POST['date'], PDO::PARAM_STR);
-        // $stmt->bindValue(":eventhr", intval($_POST['hr']), PDO::PARAM_INT);
-        // $stmt->bindValue(":eventmin", intval($_POST['min']), PDO::PARAM_INT);
-        // $stmt->bindValue(":eventabbriv", $_POST['abbriv'], PDO::PARAM_STR);
-        // $stmt->bindValue(":user_id", $_SESSION['user_id'], PDO::PARAM_INT);
+        try {
+            $stmt->bindValue(":eventname", $_POST['eventName'], PDO::PARAM_STR);
+            $stmt->bindValue(":eventdesc", $_POST['eventDesc'], PDO::PARAM_STR);
+            $stmt->bindValue(":eventdate", $_POST['date'], PDO::PARAM_STR);
+            $stmt->bindValue(":eventhr", intval($_POST['hr']), PDO::PARAM_INT);
+            $stmt->bindValue(":eventmin", intval($_POST['min']), PDO::PARAM_INT);
+            $stmt->bindValue(":eventabbriv", $_POST['abbriv'], PDO::PARAM_STR);
+            $stmt->bindValue(":user_id", $_SESSION['user_id'], PDO::PARAM_INT);
+        } 
+        catch (PDOException $ex)
+        {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+        }
 
         // $stmt->execute();
     } else {
