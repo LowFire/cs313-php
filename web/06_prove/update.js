21bd9a6b04b3af1bff80162ae8185da75de93eca
Dallin Lovin <lowfire3@yahoo.com>
2020-05-29 12:39:06 -0600

    s

---
+++ web/06_prove/update.js
@@ -0,0 +1,6 @@
+let updateButtons = document.querySelectorAll(".update");
+
+for (let i = 0; i < updateButtons.length; i++) {
+    updateButtons.addEventListener("onclick", function() {
+    });
+}
--- web/06_prove/viewEvents.php
+++ web/06_prove/viewEvents.php
@@ -3,7 +3,7 @@
   require "getDB.php";
   
   $db = getDB();
-  $stmt = $db->prepare("SELECT eventname, eventdesc, eventdate, eventhr, eventmin, eventabbriv FROM calendar WHERE user_id=:user_id");
+  $stmt = $db->prepare("SELECT event_id eventname, eventdesc, eventdate, eventhr, eventmin, eventabbriv FROM calendar WHERE user_id=:user_id");
   $stmt->bindValue(":user_id", $_SESSION['user_id'], PDO::PARAM_INT);
   $stmt->execute();
   $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -31,10 +31,12 @@
         <tbody>
           <?php
             foreach ($events as $event) {
-              printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%d:%2.0d %s</td><td>" . 
-              "<button class=\"btn btn-primary\">Update</button><button class=\"btn btn-danger\">Delete</button></td></tr>", 
+              printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%d:%2.0f %s</td><td>" . 
+              "<button class=\"btn btn-primary update\">Update</button>" .
+              "<button class=\"btn btn-danger delete\">Delete</button></td></tr>", 
               $event['eventname'], $event['eventdesc'], $event['eventdate'], $event['eventhr'],
               $event['eventmin'], $event['eventabbriv']);
+              $i++;
             }
           ?>
         </tbody>
