aaf54bbe17c0349e44de682d81a7d7ce056bb86c
Dallin Lovin <lowfire3@yahoo.com>
2020-05-29 11:30:33 -0600

    sdf

--- web/06_prove/viewEvents.php
+++ web/06_prove/viewEvents.php
@@ -1,3 +1,14 @@
+<?php
+  session_start();
+  require "getDB.php";
+  
+  $db = getDB();
+  $stmt = $db->prepare("SELECT eventname, eventdesc, eventdate, eventhr, eventmin, eventabbriv FROM calendar WHERE user_id=:user_id");
+  $stmt->bindValue(":user_id", $_SESSION['user_id'], PDO::PARAM_INT);
+  $stmt->execute();
+  $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,6 +18,25 @@
   </head>
   
   <body class="container">
-      
+    <h1>Events</h1>
+      <table class="table table-striped">
+        <thead>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th></th>
+        </thead>
+
+        <tbody>
+          <?php
+            foreach ($events as $event) {
+              printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%d:%2.0d %s</td></tr>", 
+              $event['eventname'], $event['eventdesc'], $event['eventdate'], $event['eventhr'],
+              $event['eventmin'], $event['eventabbriv']);
+            }
+          ?>
+        </tbody>
+      </table>
   </body>
 </html>
