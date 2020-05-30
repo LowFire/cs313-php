0c897cbcc67d9c1d244434e4ac8e37fb3bddb814
Dallin Lovin <lowfire3@yahoo.com>
2020-05-30 11:27:57 -0600

    d

--- web/06_prove/viewEvents.js
+++ web/06_prove/viewEvents.js
@@ -2,10 +2,22 @@ let rows = document.querySelectorAll("#eventRows > tr");
 let updateButtons = document.querySelectorAll(".update");
 let deleteButtons = document.querySelectorAll("delete");
 
+function deleteEvent() {
+
+}
+
+function cancel() {
+
+}
+
 function update() {
     this.textContent = "Save";
+    deleteButtons[this.dataset.row].textContent = "Cancel"
+    deleteButtons[this.dataset.row].addEventListener("click", cancel);
+    deleteButtons[this.dataset.row].removeEventListener("click", deleteEvent);
 }
 
 for (let i = 0; i < updateButtons.length; i++) {
     updateButtons[i].addEventListener("click", update);
+    deleteButtons[i].addEventListener("click", deleteEvent);
 }
