1ea9f3c004f3c74923946f19b041e71bb444cf91
Dallin Lovin <lowfire3@yahoo.com>
2020-05-30 11:57:37 -0600

    d

--- web/06_prove/viewEvents.js
+++ web/06_prove/viewEvents.js
@@ -12,9 +12,19 @@ function cancel() {
 
 function update() {
     this.textContent = "Save";
-    deleteButtons[this.dataset.row].textContent = "Cancel"
-    deleteButtons[this.dataset.row].addEventListener("click", cancel);
-    deleteButtons[this.dataset.row].removeEventListener("click", deleteEvent);
+    let rowNum = this.dataset.row;
+    deleteButtons[rowNum].textContent = "Cancel"
+    deleteButtons[rowNum].addEventListener("click", cancel);
+    deleteButtons[rowNum].removeEventListener("click", deleteEvent);
+    let row = document.querySelector("#eventRows tr:nth-of-type(" + (rowNum + 1) + ")");
+    row.innerHTML = "<form action=\"\" method=\"GET\">" +
+    "<td><input type=\"text\" name=\"eventname\" id=\"eventname\"></td>" +
+    "<td><input type=\"text\" name=\"eventdesc\" id=\"eventdesc\"></td>" +
+    "<td><input type=\"date\" name=\"eventdate\" id=\"eventdate\"></td>" +
+    "<td><input type=\"number\" name=\"eventhr\" id=\"eventhr\" min=\"1\" max=\"12\"></td>" +
+    "<td><input type=\"number\" name=\"eventmin\" id=\"eventmin\" min=\"0\" max=\"59\"></td>" +
+    "<td><select><option value=\"AM\">AM</option><option value=\"PM\">PM</option></select></td>" +
+    "</form>";
 }
 
 for (let i = 0; i < updateButtons.length; i++) {
