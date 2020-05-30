f3eaba17db1c742a430d8e8ada3245897c5fe0c2
Dallin Lovin <lowfire3@yahoo.com>
2020-05-30 12:14:33 -0600

    ds

--- web/06_prove/viewEvents.js
+++ web/06_prove/viewEvents.js
@@ -1,6 +1,7 @@
 let rows = document.querySelectorAll("#eventRows > tr");
 let updateButtons = document.querySelectorAll(".update");
 let deleteButtons = document.querySelectorAll(".delete");
+let rowsHTML = {};
 
 function deleteEvent() {
 
@@ -11,23 +12,25 @@ function cancel() {
 }
 
 function update() {
-    this.textContent = "Save";
     let rowNum = this.dataset.row;
-    deleteButtons[rowNum].textContent = "Cancel"
-    deleteButtons[rowNum].addEventListener("click", cancel);
-    deleteButtons[rowNum].removeEventListener("click", deleteEvent);
     let row = rows[rowNum];
     row.innerHTML = "<form action=\"\" method=\"GET\">" +
     "<td><input type=\"text\" name=\"eventname\" id=\"eventname\"></td>" +
     "<td><input type=\"text\" name=\"eventdesc\" id=\"eventdesc\"></td>" +
     "<td><input type=\"date\" name=\"eventdate\" id=\"eventdate\"></td>" +
-    "<td><input type=\"number\" name=\"eventhr\" id=\"eventhr\" min=\"1\" max=\"12\"></td>" +
-    "<td><input type=\"number\" name=\"eventmin\" id=\"eventmin\" min=\"0\" max=\"59\"></td>" +
-    "<td><select><option value=\"AM\">AM</option><option value=\"PM\">PM</option></select></td>" +
+    "<td><input type=\"number\" name=\"eventhr\" id=\"eventhr\" min=\"1\" max=\"12\">" +
+    "<input type=\"number\" name=\"eventmin\" id=\"eventmin\" min=\"0\" max=\"59\">" +
+    "<select><option value=\"AM\">AM</option><option value=\"PM\">PM</option></select></td>" +
+    "<td><button type=\"submit\" class=\"btn btn-primary\" id=\"save\">Save</button>" +
+    "<button class=\"btn btn-danger\" id=\"cancel\">Cancel</button></td>" +
     "</form>";
 }
 
 for (let i = 0; i < updateButtons.length; i++) {
     updateButtons[i].addEventListener("click", update);
     deleteButtons[i].addEventListener("click", deleteEvent);
+}
+
+for (let i = 0; i < rows.length; i++) {
+    rowsHTML[i] = rows[i].innerHTML;
 }
