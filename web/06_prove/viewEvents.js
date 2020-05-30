2f5fd04c6fb879ff2c277bdfde025b86a48367cb
Dallin Lovin <lowfire3@yahoo.com>
2020-05-30 13:23:24 -0600

    s

--- web/06_prove/viewEvents.js
+++ web/06_prove/viewEvents.js
@@ -4,26 +4,39 @@ let deleteButtons = document.querySelectorAll(".delete");
 let rowsHTML = {};
 
 function deleteEvent() {
+    console.log("delete function has run");
+}
 
+function save() {
+    console.log("Save function has run");
 }
 
 function cancel() {
-
+    console.log("cancel function has run");
 }
 
 function update() {
     let rowNum = this.dataset.row;
     let row = rows[rowNum];
     row.innerHTML = "<form action=\"\" method=\"GET\">" +
-    "<td><input type=\"text\" name=\"eventname\" id=\"eventname\"></td>" +
-    "<td><input type=\"text\" name=\"eventdesc\" id=\"eventdesc\"></td>" +
-    "<td><input type=\"date\" name=\"eventdate\" id=\"eventdate\"></td>" +
-    "<td><input type=\"number\" name=\"eventhr\" id=\"eventhr\" min=\"1\" max=\"12\">" +
-    "<input type=\"number\" name=\"eventmin\" id=\"eventmin\" min=\"0\" max=\"59\">" +
+    "<td><input type=\"text\" name=\"eventname\" id=\"eventname" + rowNum + "\"></td>" +
+    "<td><input type=\"text\" name=\"eventdesc\" id=\"eventdesc" + rowNum + "\"></td>" +
+    "<td><input type=\"date\" name=\"eventdate\" id=\"eventdate" + rowNum + "\"></td>" +
+    "<td><input type=\"number\" name=\"eventhr\" id=\"eventhr" + rowNum + "\" min=\"1\" max=\"12\">" +
+    "<input type=\"number\" name=\"eventmin\" id=\"eventmin" + rowNum + "\" min=\"0\" max=\"59\">" +
     "<select><option value=\"AM\">AM</option><option value=\"PM\">PM</option></select></td>" +
-    "<td><button type=\"submit\" class=\"btn btn-primary\" id=\"save\">Save</button>" +
-    "<button class=\"btn btn-danger\" id=\"cancel\">Cancel</button></td>" +
+    "<td id=\"buttons" + rowNum + "\"></td>" +
     "</form>";
+
+    let buttonColumn = document.querySelector("#buttons" + rowNum);
+    let saveButton = document.createElement("button");
+    let cancelButton = document.createElement("button");
+    let saveText = document.createTextNode("Save");
+    let cancelText = document.createTextNode("Cancel");
+    saveButton.appendChild(saveText);
+    cancelButton.appendChild(cancelText);
+    buttonColumn.appendChild(saveButton);
+    buttonColumn.appendChild(cancelButton);
 }
 
 for (let i = 0; i < updateButtons.length; i++) {
